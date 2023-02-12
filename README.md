48aa3c5d5b08129c4e2ef110107e1aaec660f613
SvenGarson <sven.garson@outlook.com>
2023-02-12 11:57:54 +0100

    Add initial project files

---
+++ README.md
@@ -0,0 +1,2 @@
+# nofuss-SDL2-rendering-without-graphics-api
+Using SDL2 to render into a window without using a graphics API.
---
+++ source/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+  /* TODO-GS: Implement SDL2 rendering without graphics api */
+
+  /* Return to operating system successfully */
+  return 0;
+}
